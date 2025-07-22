1495a16a4628ff845245f98acbda5fd3bfc7fa94
yashika2404 <2404yashika@gmail.com>
2025-07-22 19:38:02 +0530

    Create ReverseLinkedList.java

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,38 @@
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        head = reverseList(head);
+        printList(head);
+    }
+
+    static Node reverseList(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}
