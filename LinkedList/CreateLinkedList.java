6ad1d7d5ae35d777ca90acb3f48b482860c323cf
yashika2404 <2404yashika@gmail.com>
2025-07-22 19:36:52 +0530

    Create CreateLinkedList.java

---
+++ LinkedList/CreateLinkedList.java
@@ -0,0 +1,28 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class CreateLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        printList(head);
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
